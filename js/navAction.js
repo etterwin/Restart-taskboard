ca747ec1c27bfeba3ce641a5b7e2b6a20a8830fc
Dasha <etterwin.yll@gmail.com>
2017-10-24 17:42:29 +0300

    Update. It doesn't work. I'm desperate

--- js/navAction.js
+++ js/navAction.js
@@ -1,36 +1,6 @@
-function newColor() {
+function click() {
 
-    let res = document.getElementById('item_1').getElementsByClassName('nav-icon');
-    alert(res);
+    document.getElementById("svg_1").classList.add("nav-icon--active");
+    document.getElementById("nav_link_1").classList.add("nav-item-link--active");
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function newColor() {
-
-    //document.getElementsByClassName('nav-icon').classList.add('nav-icon--active'); //change icon's color
-    //document.getElementsByClassName('nav-item-link').classList.add('nav-item-link--active'); //change text's color
-
-//}
-
-//function removeNewColor() {
-
-    //document.getElementsByClassName("nav-icon").classList.remove("nav-icon--active"); //remove icon's color
-    //document.getElementsByClassName("nav-item-link").classList.remove("nav-item-link--active"); //change icon's color
-//}
-
