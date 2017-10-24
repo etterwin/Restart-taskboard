b467d51bffc73270038a0f946b56fa3f17e5ca0d
Dasha <etterwin.yll@gmail.com>
2017-10-24 13:05:01 +0300

    update

---
+++ js/navAction.js
@@ -0,0 +1,36 @@
+function newColor() {
+
+    let res = document.getElementById('item_1').getElementsByClassName('nav-icon');
+    alert(res);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function newColor() {
+
+    //document.getElementsByClassName('nav-icon').classList.add('nav-icon--active'); //change icon's color
+    //document.getElementsByClassName('nav-item-link').classList.add('nav-item-link--active'); //change text's color
+
+//}
+
+//function removeNewColor() {
+
+    //document.getElementsByClassName("nav-icon").classList.remove("nav-icon--active"); //remove icon's color
+    //document.getElementsByClassName("nav-item-link").classList.remove("nav-item-link--active"); //change icon's color
+//}
+
