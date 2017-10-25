75bce84af899808dc0d4ce0b4a79b62365da2be1
Dasha <etterwin.yll@gmail.com>
2017-10-25 16:24:27 +0300

    update

--- js/navAction.js
+++ js/navAction.js
@@ -8,6 +8,8 @@ $(item_1).click(function (e){
     if (item_1.is(e.target) && item_1.has(e.target).length === 0) {
         $("#svg_1").addClass("nav-icon--active");
         $("#nav_link_1").addClass("nav-item-link--active");
+        $("#nav_move").addClass("navigation-move");
+        $("#nav_move").removeClass("navigation-move--second", "navigation-move--third", "navigation-move--fourth","navigation-move--fifth");
     }
 });
 
@@ -15,7 +17,8 @@ $(item_2).click(function (e){
     if (item_2.is(e.target) && item_2.has(e.target).length === 0) {
         $("#svg_2").addClass("nav-icon--active");
         $("#nav_link_2").addClass("nav-item-link--active");
-        $("#nav_move").toggleClass("navigation-move--second");
+        $("#nav_move").addClass("navigation-move--second");
+        $("#nav_move").removeClass("navigation-move", "navigation-move--third","navigation-move--fourth","navigation-move--fifth");
     }
 });
 
@@ -23,7 +26,8 @@ $(item_3).click(function (e){
     if (item_3.is(e.target) && item_3.has(e.target).length === 0) {
         $("#svg_3").addClass("nav-icon--active");
         $("#nav_link_3").addClass("nav-item-link--active");
-        $("#nav_move").toggleClass("navigation-move--third");
+        $("#nav_move").addClass("navigation-move--third");
+        $("#nav_move").removeClass("navigation-move", "navigation-move--second","navigation-move--fourth","navigation-move--fifth");
     }
 });
 
@@ -31,7 +35,8 @@ $(item_4).click(function (e){
     if (item_4.is(e.target) && item_4.has(e.target).length === 0) {
         $("#svg_4").addClass("nav-icon--active");
         $("#nav_link_4").addClass("nav-item-link--active");
-        $("#nav_move").toggleClass("navigation-move--fourth");
+        $("#nav_move").addClass("navigation-move--fourth");
+        $("#nav_move").removeClass("navigation-move", "navigation-move--third","navigation-move--second","navigation-move--fifth");
     }
 });
 
@@ -39,7 +44,8 @@ $(item_5).click(function (e){
     if (item_5.is(e.target) && item_5.has(e.target).length === 0) {
         $("#svg_5").addClass("nav-icon--active");
         $("#nav_link_5").addClass("nav-item-link--active");
-        $("#nav_move").toggleClass("navigation-move--fifth");
+        $("#nav_move").addClass("navigation-move--fifth");
+        $("#nav_move").removeClass("navigation-move", "navigation-move--third","navigation-move--fourth","navigation-move--second");
     }
 });
 
